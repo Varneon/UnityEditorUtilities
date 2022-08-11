1393e22d2a35fc5242901e4d0adf713c57fdcea4
Varneon <me@varneon.com>
2022-08-11 02:19:49 -0700

    Add CopyFullAssetPath

---
+++ Packages/com.varneon.editor-utilities/Editor/Assets Menu Actions/CopyFullAssetPath.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+using UnityEditor;
+
+namespace Varneon.EditorUtilities.AssetsMenuActions
+{
+    public class CopyFullAssetPath : Editor
+    {
+        [MenuItem("Assets/Copy Full Path", false, 19)]
+        private static void CopyFullPath()
+        {
+            string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+
+            if (!string.IsNullOrEmpty(path) && File.Exists(path))
+            {
+                EditorGUIUtility.systemCopyBuffer = Path.GetFullPath(path);
+            }
+        }
+    }
+}
